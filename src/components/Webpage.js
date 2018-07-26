6c156794c9e6f296bbe5f005b3c880a30b7192a4
Joseph Lim <joseph.d.lim@gmail.com>
2018-07-26 21:17:41 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -5,7 +5,7 @@ class Webpage extends React.Component {
   render() {
     return(
       <div>
-        <h1>The World's Coolest Webpage</h1>
+        <h1>The World's coolest webpage</h1>
         <FillerText />
         <FillerText />
       </div>
