008b815a7c4c78f8db17ae1ef667b228ae97fba5
Joseph Lim <joseph.d.lim@gmail.com>
2018-07-26 21:06:11 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+class RegistrationForm extends Component {
+  render() {
+    return(
+      <form>
+      <input type=text;>
+      <input type=passowrd;>
+      <button type=submit;>
+      </form>
+    )
+  }
+}
